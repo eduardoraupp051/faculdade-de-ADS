74fc85aa23d2d139d1253d78b2416ef6d860190e
eduardo vicente raupp <eduardovicente73@gmail.com>
2023-05-08 14:52:07 -0300

    atualização da semana

---
+++ 1 semestre/algoritmos e programação/aula 12/lista(ex1).py
@@ -0,0 +1,10 @@
+
+lista = []
+cont = 0
+num = 1
+while cont<5:
+    lista.append(num)
+    print(f"o conteudo da lista é: {lista}")
+    cont = len(lista)
+    print(f"o tamanho da lista é: {cont}")
+    num += 1
---
+++ 1 semestre/algoritmos e programação/aula 12/lista.py
@@ -0,0 +1,11 @@
+lista = ["internacional", "nacional"]
+print(lista)
+lista2 = ["1", "2", "3", "4", "5"]
+lista3 = ["6", "7", "8", "9", "10"]
+print(lista2 + lista3)
+print(lista2)
+lista.append(6)
+print(lista)
+print(lista2[1])
+lista2.pop(4)
+print(lista2)
