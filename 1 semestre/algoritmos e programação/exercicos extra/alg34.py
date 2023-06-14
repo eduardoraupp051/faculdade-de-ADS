7a3ebaba8a4b2454ddfd67e4919cbf7543f16dad
Eduardo Vicente Raupp <91517477+duduzindo085@users.noreply.github.com>
2023-06-14 09:36:41 -0300

    Create alg34.py

---
+++ 1 semestre/algoritmos e programação/exercicos extra/alg34.py
@@ -0,0 +1,16 @@
+lista = []
+lista_impar = []
+while True:
+  num = int(input("digite alguns numeros para verificar se são impares ou digite  0 para sair: "))
+  lista.append(num)
+  if num == 0:
+    print("você esta saindo do programa")
+    break
+ for i in lista:
+    if i % 2 != 0:
+      lista_impar.append(i)
+  print(f"o numeros impares são {lista_impar}" 
+
+    
+    
+  
