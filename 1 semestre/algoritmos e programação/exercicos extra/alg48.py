a6f3f6570017f6f8918a68148e9a04c8d23a6f74
Eduardo Vicente Raupp <91517477+duduzindo085@users.noreply.github.com>
2023-06-15 12:40:20 -0300

    Create alg48.py

---
+++ 1 semestre/algoritmos e programação/exercicos extra/alg48.py
@@ -0,0 +1,16 @@
+#47
+lista = []
+
+while True:
+    num = int(input("digite numeros ou coloque 0 para sair do programa: "))
+    
+    if num == 0:
+        print("finalizando o programa")
+        break
+    else: 
+        lista.append(num)
+        
+if lista[0] > lista[1]:
+    print('esta em ordem descrescente')
+else:
+    print('esta em ordem crescente')
